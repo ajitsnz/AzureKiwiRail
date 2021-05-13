7a09c2e9bed316c77b8a16e7e40c7dc07a4f78af
Ajit <ajitsnz@gmail.com>
2021-05-14 00:50:03 +1200

    test

--- KiwiRailWebUIAutomation/Tests/Tests.cs
+++ KiwiRailWebUIAutomation/Tests/Tests.cs
@@ -7,17 +7,17 @@ namespace KiwiRailWebUIAutomation.Tests
         [Test]
         public void Tc1()
         {
-            Assert.AreEqual("test", "tes1", "fail test1");
+
         }
         [Test]
         public void Tc2()
         {
-            Assert.AreEqual("test", "tes1", "fail test2");
+
         }
         [Test]
         public void Tc3()
         {
-            Assert.AreEqual("test", "test", "pass test");
+
         }
     }
 }
