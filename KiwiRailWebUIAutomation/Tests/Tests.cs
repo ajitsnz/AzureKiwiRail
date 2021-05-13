b373561d16dff1b46251065fe3a974f1b9204098
Ajit <ajitsnz@gmail.com>
2021-05-14 00:39:34 +1200

    test

---
+++ KiwiRailWebUIAutomation/Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace KiwiRailWebUIAutomation.Tests
+{
+    class Tests
+    {
+        public void Tc1()
+        {
+            Assert.AreEqual("test", "tes1", "fail test1");
+        }
+
+        public void Tc2()
+        {
+            Assert.AreEqual("test", "tes1", "fail test2");
+        }
+
+        public void Tc3()
+        {
+            Assert.AreEqual("test", "test", "pass test");
+        }
+    }
+}
